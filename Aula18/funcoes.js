a4c945c3f65ddcda90d029c7371f14632a993915
william <william.walder@dtidigital.com.br>
2023-09-20 10:02:07 -0300

    js

---
+++ Aula18/funcoes.js
@@ -0,0 +1,11 @@
+function divisao(a, b) {
+
+    if (b == 0) {
+        console.log('Nao é possível dividir por zero')
+    } else {
+        let resultado = a / b;
+        console.log(resultado);
+    }
+}
+
+divisao(0, 20);
