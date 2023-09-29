1086ee776072e9048fddb6cbb663793feb06aa32
william <william.walder@dtidigital.com.br>
2023-09-28 21:50:36 -0300

    Bug fix aula carrinho de compras

--- Aula21/storage.js
+++ Aula21/storage.js
@@ -21,11 +21,60 @@ multiplicacao.onclick = () => {
     resultado.innerText = 'O resultado da multiplicação é: ' + operationResult;
 }
 divisao.onclick = () => {
-    if( parseInt(segundoNumero.value) != 0){
+    if (parseInt(segundoNumero.value) != 0) {
         let operationResult = parseInt(primeiroNumero.value) / parseInt(segundoNumero.value);
         resultado.innerText = 'O resultado da divisão é: ' + operationResult;
-    }else{
+    } else {
         resultado.innerText = 'O segundo número não pode ser zero';
     }
-   
+}
+
+
+
+let itemsDesejados = [];
+let itemCompra = document.getElementById("itemCompra");
+let adicionar = document.getElementById("adicionar");
+let salvar = document.getElementById("salvar");
+let visualizar = document.getElementById("visualizar");
+let apagar = document.getElementById("apagar");
+let itemsCompra = document.getElementById("itemsCompra");
+
+
+adicionar.onclick = () => {
+    itemsDesejados.push(itemCompra.value);
+    itemCompra.value = ''
+    console.log(itemsDesejados);
+}
+
+salvar.onclick = () => {
+    let itemsJson = JSON.stringify(itemsDesejados);
+    localStorage.setItem('items', itemsJson);
+}
+
+visualizar.onclick = () => {
+    let itemsJson = localStorage.getItem('items');
+    let itemsObjeto = JSON.parse(itemsJson);
+
+    if (itemsObjeto.length > 0) {
+
+        itemsObjeto.forEach(item => {
+            let itemLista = document.createElement("li");
+            itemLista.innerText = item;
+            itemsCompra.append(itemLista);
+        });
+    }
+}
+
+apagar.onclick = () => {
+    let itensLista = document.getElementsByTagName("li");
+    let quantidadeItens = itensLista.length;
+    localStorage.clear();
+    itemsDesejados = [];
+
+    if (itensLista.length > 0) {
+        
+        for(let index = 0; index < quantidadeItens; index++){
+            itensLista[0].remove();
+        }
+    }
 }
